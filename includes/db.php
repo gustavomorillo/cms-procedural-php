0c125e0eb9730b0288fd1b60576d6a4b4670e1f5
Gustavo <Gustavo@g.coms>
2019-07-01 18:25:15 -0400

    fix db 5

--- includes/db.php
+++ includes/db.php
@@ -9,23 +9,14 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
-$conn = mysqli_connect($hostname, $username, $password, $database);
+$connection = mysqli_connect($hostname, $username, $password, $database);
 
 
-if($conn) {
+if($connection) {
 	//echo "Connection succesfully";
 } else {
 	echo "Error";
 }
-
-
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
 
 
 
