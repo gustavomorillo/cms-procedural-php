7ad49792233726eaa5071d90cac29c617ccf7490
Gustavo <Gustavo@g.coms>
2019-07-01 21:39:12 -0400

    styles 7

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -20,6 +20,7 @@
     <!-- Bootstrap Core CSS -->
     
 
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
 
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
 
@@ -28,16 +29,9 @@
   <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
 
     <!-- Custom CSS -->
+    <link href="/css/sb-admin.css" rel="stylesheet">
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-    <link href="css/sb-admin.css" rel="stylesheet">
-
-
-  
-
-    <link href="/css/loader.css" rel="stylesheet"> -->
+    <link href="/css/loader.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
